91a8f0d6c7a6d394dd01e104d4a906e829d748ba
rebeka.silva <rebeka.silva.emailprofissional@gmail.com>
2025-04-22 21:42:52 -0300

    Menu semifinalizado

--- ProjetoBanco/Program.cs
+++ ProjetoBanco/Program.cs
@@ -10,6 +10,63 @@ namespace ProjetoBanco
     {
         static void Main(string[] args)
         {
+            //Menu do usuário
+
+            char opcao;
+
+            do
+            {
+
+                
+
+                Console.WriteLine("                                                            ");
+                Console.WriteLine("************************************************************");
+                Console.WriteLine("                                                            ");
+                Console.WriteLine("                          Banco JK                          ");
+                Console.WriteLine("                                                            ");
+                Console.WriteLine("            Escolha a opção da operação a ser realizada:    ");
+                Console.WriteLine("                                                            ");
+                Console.WriteLine("                  1 - Cadastrar cliente.                    ");
+                Console.WriteLine("                  2 - Inserir cliente na fila.              ");
+                Console.WriteLine("                  3 - Listar a fila                         ");
+                Console.WriteLine("                  4 - Incluir cliente prioritário           ");
+                Console.WriteLine("                  5 - Atender cliente                       ");
+                Console.WriteLine("                                                            ");
+                Console.WriteLine("************************************************************");
+                Console.WriteLine("                                                            ");
+                Console.WriteLine("                  Digite 'q' para sair.                     ");
+                Console.WriteLine("                                                            ");
+                Console.WriteLine("************************************************************");
+                opcao = char.Parse(Console.ReadLine());
+
+                switch (opcao) {
+                    case '1':
+
+                        break;
+                    case '2':
+
+                        break;
+                    case '3':
+
+                        break;
+                    case '4':
+
+                        break;
+
+                    case '5':
+
+                        break;
+
+                    case 'q':
+                        Console.WriteLine("Operação finalizada.");
+                        break;
+
+                    default:
+                        Console.WriteLine("Valor digitado inválido!");
+                        break;
+                }
+
+            } while (opcao != 'q');
         }
     }
 }
