2deb906089cbc084dc61ac6dfcfc136ee9647836
emile <emile.bispo@etec.sp.gov.br>
2025-04-22 22:13:26 -0300

    criando classe cliente

---
+++ ProjetoBanco/Cliente.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Cliente
+
+{
+	public Cliente()
+
+
+		public int id { get; set; };
+	public string nome { get; set; };
+	public string endereço { get; set; };
+	public string email { get; set; };
+	public double telefone { get; set; };
+	public double cpf { get; set; };
+}
+	public Cliente(int id, string nome, string endereço, string email, double telefone, double cpf)
+	{
+		Id = id;
+		Nome = nome;
+		Endereço = endereço;
+		Email = email;
+		Telefone = telefone;
+		Cpf = cpf;
+
+}
+	public void ExibirCadastro()
+}
+
+{ 	
+
+	 void	Console.WriteLine($ "id : {id}");
+			Console.WriteLine($ "Nome : { nome}");
+			Console.WriteLine($ "Endereço : { endereço}");
+			Console.WriteLine($ "Email : { email}");
+			Console.WriteLine($ "Telefone : { telefone}");			
+			Console.WriteLine($ "Cpf : {cpf}");
+		}
