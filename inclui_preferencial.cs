9800470ab0aba1ab59311b0564ed48702fc37759
tkiilian21 <thiago.kulian@etec.sp.gov.br>
2025-04-28 19:26:15 -0300

    Classe inclui cliente prioritario

---
+++ inclui_preferencial.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class inclui_preferencial
+{
+    public void IncluiPreferencial(string nome)
+    
+        if (count<tamanhoFila)
+        {
+            inicio = (inicio - 1 + tamanhoFila) % tamanhoFila;
+            fila[inicio] = nome;
+            count++;
+            Console.WriteLine("Cliente Preferencial {0} incluido na fila", nome);
+        }
+        else
+{
+    Console.WriteLine("Fila cheia! Não é possível adicionar clientes preferemc.");
+}
+    }
+}
